94a7bd7068ce45298aae92ea3a1c7f3801ff8fd1
qalexball <zhuderen12345@icloud.com>
2021-02-06 01:52:21 +0800

    update at 2021-02-06 [1:51:55]

--- routes/api/auth.go
+++ routes/api/auth.go
@@ -1,1 +1,33 @@
 package api
+
+import (
+	"gin_blog/models"
+	"gin_blog/utils/e"
+
+	"github.com/astaxie/beego/validation"
+	"github.com/gin-gonic/gin"
+)
+
+type auth struct {
+	Username string `valid:"Required; MaxSize(50)"`
+	Password string `valid:"Required; MaxSize(50)"`
+}
+
+// GetAuth => get auth api
+func GetAuth(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+
+	valid := validation.Validation{}
+	a := auth{Username: username, Password: password}
+	ok, _ := valid.Valid(&a)
+
+	data := make(map[string]interface{})
+	code := e.INVALID_PARAMS
+	if ok {
+		isExist, err := models.CheckAuth(username, password)
+		if isExist {
+
+		}
+	}
+}
